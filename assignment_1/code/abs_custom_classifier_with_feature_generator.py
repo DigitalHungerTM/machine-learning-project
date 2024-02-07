250f65c03cd800a6f812f65b389481f9034983e1
Mathijs Afman <afman.mathijs@gmail.com>
2024-02-07 21:13:45 +0100

    moved vocab generator and ngram-ifier to their own methods

--- assignment_1/code/abs_custom_classifier_with_feature_generator.py
+++ assignment_1/code/abs_custom_classifier_with_feature_generator.py
@@ -18,17 +18,41 @@ class CustomClassifier(abc.ABC):
     def __init__(self):
         self.counter = None
 
-    def get_features(self, text_list: list[list[str]], n=1):
+    def gen_vocab(self, text_list: list[list[str]], n):
         """
-        :param `text_list`: list of preprocessed, tokenized tweets
-        :param `n`: length of gram in N-Hot encoded array, default 1
-        :return `features_array`: 2D, N-Hot encoded numpy array of features per tweet
-        :return `vocab`: array of unique ngrams in all of text_list
-        """
-        assert n >= 1, f'{self.get_features.__qualname__}: n should be 1 or larger'
+        generates a vocab for a list of tweets according to the ngram value
         
+        :param `text_list`: list of tokenized tweets
+        :param `n`: number of tokens in ngram, defaults to 1
+        :return `vocab`: ngram vocabulary of the tweets list
+        """
+
         # generate vocabulary and convert tweets to ngrams
         vocab = set()
+
+        for tweet in text_list:
+            length = len(tweet)
+            i = 0
+            
+            while i < length - n: # make sure we don't run out of space
+
+                ngram = tuple(tweet[i:i+n])
+                vocab.add(ngram) # tuples are ordered and unmutable
+                
+                i += 1
+            
+        return tuple(vocab) # tuples are ordered and unmutable
+    
+    def n_gram_ify(self, text_list: list[list[str]], n):
+        """
+        'ngram-ifies' the tweets in the `text_list`
+        
+        :param `text_list`: list of tweets
+        :param `n`: number of tokens in ngram, defaults to 1
+        :return `ngram_tweet_list`: list of ngram-ified tweets
+        """
+
+        # generate vocabulary and convert tweets to ngrams
         ngram_tweet_list = []
 
         for tweet in text_list:
@@ -40,14 +64,27 @@ class CustomClassifier(abc.ABC):
 
                 ngram = tuple(tweet[i:i+n])
                 ngram_tweet.append(ngram)
-                vocab.add(ngram) # tuples are ordered and unmutable
                 
                 i += 1
             
             ngram_tweet_list.append(ngram_tweet)
+        
+        return ngram_tweet_list
 
-        vocab = tuple(vocab) # tuples are ordered and unmutable
-        features_array = np.zeros(shape=(len(text_list), len(vocab))) # make a 2D matrix filled with zeros
+    def get_features(self, text_list: list[list[str]], n=1):
+        """
+        :param `text_list`: list of preprocessed, tokenized tweets
+        :param `n`: length of gram in N-Hot encoded array, default 1
+        :return `features_array`: 2D, N-Hot encoded numpy array of features per tweet
+        :return `vocab`: array of unique ngrams in all of text_list
+        """
+        assert n >= 1, f'{self.get_features.__qualname__}: n should be 1 or larger'
+        
+        # generate vocabulary and convert tweets to ngrams
+        vocab = self.gen_vocab(text_list, n)
+        ngram_tweet_list = self.n_gram_ify(text_list, n)
+
+        features_array = np.zeros(shape=(len(ngram_tweet_list), len(vocab))) # make a 2D matrix filled with zeros
 
         # loop over vocab and tweets in order of `features_array` dimensions
         for vocab_index, vocab_ngram in enumerate(vocab):
