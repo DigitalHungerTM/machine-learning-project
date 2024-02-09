a72cb6d8c8410593f68fd2f38eb71bd2f6b9c111
Mathijs Afman <afman.mathijs@gmail.com>
2024-02-09 17:07:06 +0100

    grapher initial commit

---
+++ assignment_1/code/reporting/grapher.py
@@ -0,0 +1,16 @@
+
+
+
+
+def main():
+    # TODO:
+     # write data in csv or tsv config
+     # might use the provided data reader for that
+     # only want accuracy and macro f1-score
+     # format:
+     # n, k, accuracy, macro_f1
+    pass
+
+
+if __name__ == "__main__":
+    main()
