f6b6e735d32f1c3eba2516ef99d4ee4cef506063
DigitalHungerTM <afman.mathijs@gmail.com>
2024-02-15 12:07:30 +0100

    data

--- assignment_1/code/reporting/grapher.py
+++ assignment_1/code/reporting/grapher.py
@@ -1,15 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
+def read_dataset(filename: str):
+    """
+    read csv file and return it as pandas dataframe.
+    it takes the first line of the file as column names
+    :param `filename`: name of the file (excluding extension)
+    :return `inputdf`: pandas df of the data
+    """
+    with open(f'{filename}', encoding='utf-8') as infile:
+        inputdf = pd.read_csv(infile, sep=",", encoding='utf-8', header=0)
+
+    return inputdf
 
 
 def main():
-    # TODO:
-     # write data in csv or tsv config
-     # might use the provided data reader for that
-     # only want accuracy and macro f1-score
-     # format:
-     # n, k, accuracy, macro_f1
-    pass
+    df = read_dataset("reporting/n_fold_new.csv")
+    metric = 'euclidean'
+    # for n in range(1,5):
+    #     new_df = df[((df.distance_metric == metric) & (df.n == n))][['k', 'avg_macro_f1']]
+    #     plt.plot(new_df.k, new_df.avg_macro_f1, label=f'{n=}')
+    # plt.title(f'{metric.title()} n-fold validation')
+    # plt.xlabel('k')
+    # plt.ylabel('avg macro f1')
+    # plt.legend()
+    # plt.pause(0.01)
+    # plt.savefig(f'reporting/{metric}_n_fold.png')
+    # plt.show()
+
+    # metrics = ['euclidean', 'cosine']
+    # for metric in metrics:
+    #     new_df = df[((df.distance_metric == metric) & (df.n == 4))][['k', 'avg_macro_f1']]
+    #     plt.plot(new_df.k, new_df.avg_macro_f1, label=f'{metric}')
+
+    # plt.title(f'Best n for both metrics')
+    # plt.xlabel('k')
+    # plt.ylabel('avg macro f1')
+    # plt.legend()
+    # plt.pause(0.01)
+    # plt.savefig(f'reporting/best_n_comparison.png')
+    # plt.show()
+
+    df_token = read_dataset("reporting/n_fold_tokens.csv")
+    df_char = read_dataset("reporting/n_fold_new.csv")
+    # for n in range(1, 5):
+    #     new_df = df_token[((df_token.distance_metric == metric) & (df_token.n == n))][['k', 'avg_macro_f1']]
+    #     plt.plot(new_df.k, new_df.avg_macro_f1, label=f'{n=}')
+
+    for metric in ['euclidean', 'cosine']:
+        new_df_token = df_token[((df_token.distance_metric == metric) & (df_token.n == 1))]
+        plt.plot(new_df_token.k, new_df_token.avg_macro_f1, label=f'best token based with {metric}')
+    for metric in ['euclidean', 'cosine']:
+        new_df_char = df_char[((df_char.distance_metric == metric) & (df_char.n == 4))]
+        plt.plot(new_df_char.k, new_df_char.avg_macro_f1, label=f'best character based with {metric}')
+
+    plt.legend()
+    plt.title(f'Token vs character, best n')
+    plt.xlabel('k')
+    plt.ylabel('avg macro f1')
+    plt.savefig(f'reporting/token_character_comparison.png')
+    plt.show()
 
 
 if __name__ == "__main__":
